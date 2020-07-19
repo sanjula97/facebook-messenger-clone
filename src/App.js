a5c677b3c0792b4d037ab714d42820a678583f40
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-19 20:44:29 +0530

    Added the landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
 import "./App.css";
 
 function App() {
-  return <div className="app">Hello sanjula</div>;
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
+  console.log(messages);
+  return (
+    <div className="app">
+      <h1>Facebook messenger</h1>
+
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          onClick={sendMessage}
+          disabled={!input}
+        >
+          Send message
+        </Button>
+      </form>
+
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
